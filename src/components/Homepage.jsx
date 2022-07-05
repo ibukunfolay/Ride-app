5157bbbc3869fcbc88658ffad8873869318ff7f4
ibukunfolay <70657237+ibukunfolay@users.noreply.github.com>
2022-07-05 22:53:14 +0100

    update

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -55,7 +55,10 @@ const Homepage = () => {
         </div>
       </nav>
       <div className="flex flex-wrap lg:flex-nowrap w-full lg:pl-[59px] lg:pr-[67px] items-center">
-        <div className="flex flex-col lg:w-[511px] w-full" data-aos="fade-up">
+        <div
+          className="flex flex-col lg:w-[511px] lg:mr-[1em] w-full"
+          data-aos="fade-up"
+        >
           <h2 className="font-cerap font-bold lg:text-[48px] text-center lg:text-justify text-[32px] leading-[53px] tracking-[-0.01em] text-[#232c35] mb-4 ">
             Compare & book
             <br />
